182b3d9dcb36ed2e2d5a5fc8b277805357943035
khanmcodes <kkhalepoto@gmail.com>
2025-05-20 19:26:26 +0500

    Password handling for google signup

--- server/models/User.js
+++ server/models/User.js
@@ -12,7 +12,9 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: true
+    required: function() {
+      return !this.googleId;
+    }
   },
   googleId: {
     type: String,
